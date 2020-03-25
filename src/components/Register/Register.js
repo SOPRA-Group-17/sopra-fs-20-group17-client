4c821bff992fe21015ab85d3ab965b82ba8a37ab
19lenny <lennartlou.jung@uzh.ch>
2020-03-25 14:01:19 +0100

    test

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -157,6 +157,7 @@ class Register extends React.Component {
 
                                 onClick={() => {
                                     this.register();
+                                    
                                 }}
                             >
                                 Register
