8191a0447c6c7c3164d04ac04ceb0ae7f69b9327
zuercher8353 <zuercher8353@gmail.com>
2020-04-16 10:19:31 +0200

    updated giveClue, to new api specs FS20G17-49

--- src/components/GiveClue/GiveClue.js
+++ src/components/GiveClue/GiveClue.js
@@ -16,8 +16,8 @@ class GiveClue extends React.Component {
       word: null,
       clue: null,
       gameId: null,
-      token: null
-
+      token: null,
+      timer: null,
     };
   }
 
@@ -27,12 +27,42 @@ class GiveClue extends React.Component {
       this.state.id = localStorage.getItem("Id");
       this.state.token = localStorage.getItem("token");
 
+      this.checkTermAvailible();
+
+      this.timer = setInterval(() => this.checkTermAvailible(), 2000);
+    } catch (error) {
+      alert(
+        `Something went wrong while getting the term: \n${handleError(error)}`
+      );
+    }
+  }
+
+  async checkTermAvailible() {
+    try {
+      const response = await api.get(`/games/${this.state.gameId}`);
+
+      // check if game ready to give hints
+      console.log(response.data.status);
+      if (response.data.status === "RECEIVINGHINTS") {
+        clearInterval(this.timer);
+        this.timer = null;
+        this.getTerme();
+      }
+    } catch (error) {
+      alert(
+        `Something went wrong while checking the game status: \n${handleError(
+          error
+        )}`
+      );
+    }
+  }
+
+  async getTerme() {
+    try {
       const response = await api.get(`/games/${this.state.gameId}/terms`);
 
       // Get the returned users and update the state.
       this.setState({ word: response.data });
-
-    
     } catch (error) {
       alert(
         `Something went wrong while getting the term: \n${handleError(error)}`
@@ -43,13 +73,15 @@ class GiveClue extends React.Component {
   async submitClue() {
     try {
       const requestBody = JSON.stringify({
-        userToke: this.state.token,
-        content: this.state.clue
+        content: this.state.clue,
+        token: this.state.token,
       });
       console.log(this.state.newGame);
-      const response = await api.post(`/games/${this.state.gameId}/hints`, requestBody);
+      const response = await api.post(
+        `/games/${this.state.gameId}/hints`,
+        requestBody
+      );
 
-      
       console.log(response);
 
       this.props.history.push(`/game/${this.state.gameId}/validation`);
@@ -66,66 +98,74 @@ class GiveClue extends React.Component {
 
   render() {
     return (
-      
       <Container fluid>
         {!this.state.users ? (
-          <Spinner />
+          <div>
+            <Spinner />
+            <p>Waiting for the word to guess</p>
+          </div>
         ) : (
           <div>
-        <Row>
-          <Col xs="5" md="3">
-            <img className="logoImgSmall" src={logo} alt="Just One Logo"></img>
-          </Col>
-          <Col xs={{ span: 3, offset: 4 }} md={{ span: 2, offset: 7 }}>
-            <Row className="d-flex justify-content-end">
-              <Button
-                variant="outline-light"
-                className="outlineWhite-Dashboard"
-                size="lg"
-              >
-                Rules
-              </Button>
+            <Row>
+              <Col xs="5" md="3">
+                <img
+                  className="logoImgSmall"
+                  src={logo}
+                  alt="Just One Logo"
+                ></img>
+              </Col>
+              <Col xs={{ span: 3, offset: 4 }} md={{ span: 2, offset: 7 }}>
+                <Row className="d-flex justify-content-end">
+                  <Button
+                    variant="outline-light"
+                    className="outlineWhite-Dashboard"
+                    size="lg"
+                  >
+                    Rules
+                  </Button>
+                </Row>
+              </Col>
             </Row>
-          </Col>
-        </Row>
-        <div
-          style={{ fontSize: "calc(1.2em + 4vw)" }}
-          class="row justify-content-center"
-        >
-          <p style={{ fontFamily: "Merienda, cursive" }}>{this.state.word}</p>
-        </div>
-        <div class="row justify-content-center">
-          <input
-            style={{
-              backgroundColor: "#291f33",
-              border: "0.05vw solid white",
-              padding: "0.5vw 0.5vw 0.5vw  1vw ",
-              color: "white",
-              width: "calc(8em + 23vw)",
-              fontSize: "calc(1em + 1vw)",
-            }}
-            placeholder="Enter here your clue"
-            onChange={(e) => {
-              this.handleInputChange("clue", e.target.value);
-            }}
-          />
-        </div>
-        <div
-          class="row justify-content-center"
-          style={{ marginTop: "calc(1.5em + 1.5vw)" }}
-        >
-          <Button
-            disabled={!this.state.clue}
-            variant="outline-light"
-            size="lg"
-            onClick={() => {
-              this.submitClue();
-            }}
-          >
-            Submit
-          </Button>
-        </div>
-        </div>
+            <div
+              style={{ fontSize: "calc(1.2em + 4vw)" }}
+              class="row justify-content-center"
+            >
+              <p style={{ fontFamily: "Merienda, cursive" }}>
+                {this.state.word}
+              </p>
+            </div>
+            <div class="row justify-content-center">
+              <input
+                style={{
+                  backgroundColor: "#291f33",
+                  border: "0.05vw solid white",
+                  padding: "0.5vw 0.5vw 0.5vw  1vw ",
+                  color: "white",
+                  width: "calc(8em + 23vw)",
+                  fontSize: "calc(1em + 1vw)",
+                }}
+                placeholder="Enter here your clue"
+                onChange={(e) => {
+                  this.handleInputChange("clue", e.target.value);
+                }}
+              />
+            </div>
+            <div
+              class="row justify-content-center"
+              style={{ marginTop: "calc(1.5em + 1.5vw)" }}
+            >
+              <Button
+                disabled={!this.state.clue}
+                variant="outline-light"
+                size="lg"
+                onClick={() => {
+                  this.submitClue();
+                }}
+              >
+                Submit
+              </Button>
+            </div>
+          </div>
         )}
       </Container>
     );
