b954b1747f491816aef2f2f74e48b3e22ffaea12
markus99b <55450065+markus99b@users.noreply.github.com>
2020-04-20 16:31:38 +0200

    report word screen done
    FS20G17-47

--- src/components/ReportWord/ReportWord.js
+++ src/components/ReportWord/ReportWord.js
@@ -5,6 +5,25 @@ import { withRouter } from "react-router-dom";
 import Player from "../shared/models/Player";
 import { Container, Row, Col, Button, Table } from "react-bootstrap";
 import logo from "../styling/JustOne_logo_white.svg";
+import { Spinner } from "../../views/design/Spinner";
+
+const word = {
+  fontSize: "7vw",
+  textAlign: "center",
+  color: "white",
+};
+
+const question = {
+  fontSize: "3vw",
+  textAlign: "center",
+  opacity: 0.2,
+};
+
+const sentence = {
+  fontSize: "2vw",
+  textAlign: "center",
+  opacity: 0.2,
+};
 
 class ReportWord extends React.Component {
   constructor() {
@@ -15,10 +34,7 @@ class ReportWord extends React.Component {
       playerId: null,
       gameId: null,
       roundId: null,
-
-      hints: ["yellow", "hot", "shine", "light"],
-      //hints: [],
-      guess: null,
+      word: "sun",
     };
   }
 
@@ -30,47 +46,111 @@ class ReportWord extends React.Component {
       this.state.gameId = this.props.match.params.gameId;
       this.state.roundId = this.props.match.params.roundId;
 
-      // get all the given hints
-      // TODO: adapt the url
-      const response = await api.get(
-        `/games/${this.state.gameId}/rounds/${this.state.roundId}/actions`
-      );
+      // get the term which has to be guessed
+      /*
+      const response = await api.get(`/games/${this.state.gameId}/terms`);
       console.log(response);
-      this.setState({ hints: response });
+      this.setState({ word: response });*/
     } catch (error) {
       alert(
-        `Something went wrong while loading the player's hints: \n${handleError(
+        `Something went wrong while getting the word which has to be guessed: \n${handleError(
           error
         )}`
       );
     }
   }
 
+  async yes() {
+    try {
+      // is this api correct???
+      const response = await api.post(`/games/${this.state.gameId}/hints`);
+      console.log(response);
+    } catch (error) {
+      alert(`Something went wrong while reporting: \n${handleError(error)}`);
+    }
+  }
+
+  async no() {
+    try {
+      // is this api correct?
+      const response = await api.delete(`/games/${this.state.gameId}/hints`);
+      console.log(response);
+    } catch (error) {
+      alert(`Something went wrong while reporting: \n${handleError(error)}`);
+    }
+  }
+
   render() {
     return (
-      <div>
-        <Container fluid>
-          <Row>
-            <Col xs="5" md="3">
-              <img
-                className="logoImgSmall"
-                src={logo}
-                alt="Just One Logo"
-              ></img>
-            </Col>
-            <Col xs={{ span: 3, offset: 4 }} md={{ span: 2, offset: 7 }}>
-              <Row className="d-flex justify-content-end">
+      <Container fluid>
+        <Row>
+          <Col xs="5" md="3">
+            <img className="logoImgSmall" src={logo} alt="Just One Logo"></img>
+          </Col>
+          <Col xs={{ span: 3, offset: 4 }} md={{ span: 2, offset: 7 }}>
+            <Row className="d-flex justify-content-end">
+              <Button
+                variant="outline-light"
+                className="outlineWhite-Dashboard"
+              >
+                Rules
+              </Button>
+            </Row>
+          </Col>
+        </Row>
+
+        {!this.state.word ? (
+          <Spinner />
+        ) : (
+          <div>
+            <Row>
+              <Col>
+                <p style={word}>{this.state.word}</p>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <p style={question}>Do you know the word?</p>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
                 <Button
-                  variant="outline-light"
+                  variant="outline-success"
                   className="outlineWhite-Dashboard"
+                  onClick={() => {
+                    this.yes();
+                  }}
                 >
-                  Rules
+                  Yes
                 </Button>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+              </Col>
+
+              <Col>
+                <Button
+                  variant="outline-danger"
+                  className="outlineWhite-Dashboard"
+                  onClick={() => {
+                    this.no();
+                  }}
+                >
+                  No
+                </Button>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <p style={sentence}>
+                  Number of players that don't know the word
+                </p>
+              </Col>
+            </Row>
+          </div>
+        )}
+      </Container>
     );
   }
 }
